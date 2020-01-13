2db68fbe3c73d15daf647cf01888e7c8cebf95fc
Oscar Polanco <opolanco@intellisys.com.do>
2020-01-13 18:56:58 -0400

    Add a variable and use php on different parts of the file

--- experiment.php
+++ experiment.php
@@ -1,3 +1,6 @@
-<?php echo 2 + 2 ?>
-<h1>This page is all about brad</h1>
-<h2>All about brad</h2>
+<?php echo 2 + 2;
+    $myname = 'Brad';
+?>
+<h1>This page is all about <?php echo $myname ?></h1>
+<?php echo 5 * 5 ?>
+<h2>All about <?php echo $myname ?></h2>

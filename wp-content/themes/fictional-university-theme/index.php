68b4a31b97d541d4773541570943e4e23f5dbbc4
Oscar Polanco <opolanco@intellisys.com.do>
2020-01-22 18:54:44 -0400

    Another basic loop and array example

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,13 +1,11 @@
 <?php
     $names = array('Brad', 'john', 'Jane', 'Test');
 
-    $count = 1;
-    while($count < 101) {
-        echo "<li>$count</li>";
+    $count = 0;
+    while($count < count($names)) {
+        echo "<li>Hi, my name is $names[$count].</li>";
         $count++;
     }
 ?>
-
-<p>His, my name is <?php echo $names[0]; ?>.</p>
 
 

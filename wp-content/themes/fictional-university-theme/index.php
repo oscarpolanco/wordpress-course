1bbaadffc6ae17f0ba5882d3d6cafa9be3bd003f
Oscar Polanco <opolanco@intellisys.com.do>
2020-02-05 18:49:09 -0400

    Add a loop to see all post or a single post

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,11 +1,12 @@
 <?php
-    $names = array('Brad', 'john', 'Jane', 'Test');
 
-    $count = 0;
-    while($count < count($names)) {
-        echo "<li>Hi, my name is $names[$count].</li>";
-        $count++;
-    }
+    while(have_posts()) {
+        the_post(); 
+?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php the_content(); ?>
+        <hr>
+<?php }
 ?>
 
 

058ab69da3fde65224c717d2fe804dae94d7fbc1
Oscar Polanco <opolanco@intellisys.com.do>
2020-03-13 19:25:12 -0400

    Remove menus from the admin and use custom template menus with the highlight functionallity

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -12,9 +12,6 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_feature()
 {
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocation1', 'Footer Location One');
-    register_nav_menu('footerLocation2', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
